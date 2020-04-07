5fc658b358d61c2aacd7854b4fc2f6434a6366d9
Mark A Guza <guzamaguza@gmail.com>
2020-04-07 19:27:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_044451) do
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
